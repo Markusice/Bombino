528e575695158909686092d165d4d0bd91465e99
mark.szekeres <113556011+Markusice@users.noreply.github.com>
2024-03-14 23:13:59 +0100

    Name scenes in snake_case and create 'rounds_menu' scene

---
+++ scripts/ui/NumberInput.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+public partial class NumberInput : LineEdit
+{
+    private void OnTextChanged(string newText)
+    {
+    }
+
+    private void OnTextSubmitted(string newText)
+    {
+    }
+}
