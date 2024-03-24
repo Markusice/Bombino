8a52d5ab578d78a2cd0ec96e6734d242d21fed5a
adam.bugar <y44d2b@inf.elte.hu>
2024-03-24 16:34:36 +0100

    Write enemy directions

---
+++ scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿namespace Bombino.scripts;
+
+using System;
+using Godot;
+
+internal partial class Enemy : CharacterBody3D
+{
+    #region Signals
+
+    [Signal]
+    public delegate void HitEventHandler();
+
+    #endregion
+
+    public int Speed { get; set; } = 10;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var directions = new Vector3[]
+        {
+            Vector3.Forward,
+            Vector3.Back,
+            Vector3.Left,
+            Vector3.Right
+        };
+
+        var randomDirection = GetRandomDirection(directions);
+
+
+    }
+
+    private Vector3 GetRandomDirection(Vector3[] directions)
+    {
+        var randomIndex = new Random().Next(0, directions.Length);
+
+        return directions[randomIndex];
+    }
+}
