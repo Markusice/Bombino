1ff3fd0ee430002ec148319dfb3aa1a088b02b57
Szekeres Márk <tkkkas@inf.elte.hu>
2024-04-14 00:29:58 +0200

    Set PowerUp's MeshInstance3D Material based on its type

--- power_up/PowerUp.cs
+++ power_up/PowerUp.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using Bombino.game.persistence.state_storage;
 using Bombino.player;
 using Godot;
 
@@ -8,18 +7,32 @@ namespace Bombino.power_up;
 
 internal partial class PowerUp : Area3D
 {
+    [Export(PropertyHint.File, ".tres")] private Material _hologramPink;
+    [Export(PropertyHint.File, ".tres")] private Material _hologramBlue;
+
     private PowerUpType _type;
+    private MeshInstance3D _meshInstance3D;
 
     public override void _Ready()
     {
+        _meshInstance3D = GetNode<MeshInstance3D>("%MeshInstance3D");
         _type = GetRandomPowerUpType();
+
+        SetMaterialBasedOnType();
     }
 
-    private PowerUpType GetRandomPowerUpType()
+    private static PowerUpType GetRandomPowerUpType()
     {
         var values = Enum.GetValues(typeof(PowerUpType)).Cast<PowerUpType>().ToArray();
 
         return values[new Random().Next(values.Length)];
+    }
+
+    private void SetMaterialBasedOnType()
+    {
+        var material = _type == PowerUpType.IncreaseBombRange ? _hologramBlue : _hologramPink;
+
+        _meshInstance3D.SetSurfaceOverrideMaterial(0, material);
     }
 
     private void OnBodyEntered(Node3D body)
@@ -27,7 +40,7 @@ internal partial class PowerUp : Area3D
         if (!body.IsInGroup("players"))
             return;
 
-        var player = body as Player;
+        var player = (Player)body;
 
         if (_type == PowerUpType.IncreaseMaxBombs)
             player.PlayerData.MaxNumberOfAvailableBombs++;
