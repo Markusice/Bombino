b86c341cc5381f67c86ae0a099d527bf03d4b34c
Szekeres Márk <tkkkas@inf.elte.hu>
2024-04-16 19:25:18 +0200

    Write docs for this class

--- events/Events.cs
+++ events/Events.cs
@@ -1,18 +1,35 @@
-﻿using Bombino.player;
-using Godot;
+﻿using Godot;
 
 namespace Bombino.events;
 
+/// <summary>
+/// Event bus for distant nodes to communicate using signals.
+/// This is intended for cases where connecting the nodes directly creates more coupling
+/// or increases code complexity substantially.
+/// </summary>
 internal sealed partial class Events : Node
 {
     #region Signals
 
+    /// <summary>
+    /// Emitted when a player dies.
+    /// <param name="playerColor">to identify player's player_name and bomb_status container</param>
+    /// </summary>
     [Signal]
     public delegate void PlayerDiedEventHandler(string playerColor);
 
+    /// <summary>
+    /// Event that is triggered when the number of available bombs for a player is incremented.
+    /// <param name="playerColor">to identify player's player_name and bomb_status container</param>
+    /// <paramref name="numberOfAvailableBombs"/>
+    /// </summary>
     [Signal]
     public delegate void PlayerBombNumberIncrementedEventHandler(string playerColor, int numberOfAvailableBombs);
 
+    /// <summary>
+    /// Event that is triggered when the number of available bombs for a player is decreased.
+    /// <param name="playerColor">to identify player's player_name and bomb_status container</param>
+    /// </summary>
     [Signal]
     public delegate void PlayerBombNumberDecreasedEventHandler(string playerColor, int numberOfAvailableBombs);
 
