b0a3f9fc77c26ec6ae2027f0c1af063a75d861db
Szekeres Márk <tkkkas@inf.elte.hu>
2024-04-23 00:19:45 +0200

    Write EnterKeyPressed checker

--- ui/scripts/InputEventChecker.cs
+++ ui/scripts/InputEventChecker.cs
@@ -5,7 +5,7 @@ namespace Bombino.ui.scripts;
 /// <summary>
 /// Provides methods for checking input events.
 /// </summary>
-internal class InputEventChecker
+internal abstract class InputEventChecker
 {
     /// <summary>
     /// Checks if the Escape key is pressed in the given input event.
@@ -15,5 +15,15 @@ internal class InputEventChecker
     public static bool IsEscapeKeyPressed(InputEvent @event)
     {
         return @event is InputEventKey { Pressed: true, Keycode: Key.Escape };
+    }
+
+    /// <summary>
+    /// Checks if the Enter key is pressed in the given input event.
+    /// </summary>
+    /// <param name="event">The input event to check.</param>
+    /// <returns>True if the Enter key is pressed, false otherwise.</returns>
+    public static bool IsEnterKeyPressed(InputEvent @event)
+    {
+        return @event is InputEventKey { Pressed: true, Keycode: Key.Enter };
     }
 }
