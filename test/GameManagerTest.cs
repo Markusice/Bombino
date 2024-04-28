0968dad1278a049c17efb4e462892eae7478229e
andras.burus <rn7waa@inf.elte.hu>
2024-04-29 01:06:54 +0200

    Refactor test classes and add GameManagerTest

---
+++ test/GameManagerTest.cs
@@ -0,0 +1,74 @@
+using System.Threading.Tasks;
+using Godot;
+using Chickensoft.GoDotTest;
+using Chickensoft.GoDotLog;
+using GodotTestDriver;
+using GodotTestDriver.Drivers;
+using NUnit.Framework;
+using Shouldly;
+using Bombino.game;
+
+namespace Bombino.test;
+
+public class GameManagerTest : TestClass {
+  private readonly ILog _log = new GDLog(nameof(GameManagerTest));
+
+  private Node _mainNode = default!;
+  private GameManager _game = default!;
+  private Fixture _fixture = default!;
+
+  public GameManagerTest(Node testScene) : base(testScene) { }
+
+  [SetupAll]
+  public async Task SetupAll() 
+  {
+    _log.Print("Setup everything");
+    _fixture = new Fixture(TestScene.GetTree());
+    _mainNode = await _fixture.LoadAndAddScene<Node>("res://game/game.tscn");
+    _game = _mainNode.GetChild<GameManager>(0);
+    
+  } 
+
+  [Setup]
+  public void SetupGameManagerIsNull()
+  {
+  _log.Print("Setup");
+  }
+
+  [Test]
+  public void TestGameManagerIsNull()
+  {
+    _game.ShouldNotBeNull();
+  } 
+
+  [Cleanup]
+  public void CleanupGameManagerIsNull() 
+  {
+    _log.Print("Cleanup");
+  }
+
+  [Setup]
+  public void SetupGameMapIsNull_AfterGameStart()
+  {
+    _game.CreateNewGame();
+  }
+
+  [Test]
+  public void TestGameMapIsNull_AfterGameStart()
+  {
+    GameManager.GameMap.ShouldNotBeNull();
+  } 
+
+  [Cleanup]
+  public void CleanupGameMapIsNull_AfterGameStart() 
+  {
+    _log.Print("Cleanup");
+  } 
+
+  [CleanupAll]
+  public void CleanupAll() => _fixture.Cleanup();
+
+  [Failure]
+  public void Failure() =>
+    _log.Print("Runs whenever any of the tests in this suite fail.");
+}
