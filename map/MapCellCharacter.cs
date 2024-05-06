2d0830d09ff0c1b50afed452ab7706f2a0cfabbe
Szekeres Márk <tkkkas@inf.elte.hu>
2024-05-06 23:39:49 +0200

    Add enum for map cell characters

---
+++ map/MapCellCharacter.cs
@@ -0,0 +1,13 @@
+﻿namespace Bombino.map;
+
+internal enum MapCellCharacter
+{
+    Empty = '0',
+    Wall = 'W',
+    Floor = 'F',
+    Crate = 'C',
+    BluePlayer = 'B',
+    RedPlayer = 'R',
+    YellowPlayer = 'Y',
+    Enemy = 'E'
+}
