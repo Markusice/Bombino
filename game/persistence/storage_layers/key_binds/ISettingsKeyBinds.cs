31697ce85df0705f3339adf7ed2a08b22215d813
Szekeres Márk <tkkkas@inf.elte.hu>
2024-05-13 01:28:47 +0200

    write docs for interface

--- game/persistence/storage_layers/key_binds/ISettingsKeyBinds.cs
+++ game/persistence/storage_layers/key_binds/ISettingsKeyBinds.cs
@@ -5,7 +5,23 @@ namespace Bombino.game.persistence.storage_layers.key_binds;
 /// </summary>
 internal interface ISettingsKeyBinds
 {
+    /// <summary>
+    /// Saves the key binds data.
+    /// </summary>
+    /// <returns>True if the data was successfully saved; otherwise, false.</returns>
     bool SaveKeyBinds();
 
+    /// <summary>
+    /// Loads the key binds data.
+    /// </summary>
+    /// <returns>True if the data was successfully loaded; otherwise, false.</returns>
     bool LoadKeyBinds();
+
+    /// <summary>
+    /// Checks if the key binds data is loaded.
+    /// </summary>
+    /// <returns>
+    /// True if the key binds data is loaded; otherwise, false.
+    /// </returns>
+    bool IsLoaded();
 }
