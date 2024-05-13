9cf03263cab34216c0c75c333516e8278c047b2b
Bugár Ádám <y44d2b@inf.elte.hu>
2024-05-13 22:26:40 +0200

    Add documentation to Animation Scripts

--- ui/main_ui/animation_scripts/PlayerBombNumberChangeAnimation.cs
+++ ui/main_ui/animation_scripts/PlayerBombNumberChangeAnimation.cs
@@ -3,6 +3,9 @@ using Godot;
 
 namespace Bombino.ui.main_ui.animation_scripts;
 
+/// <summary>
+/// Represents the player bomb number change animation scene.
+/// </summary>
 internal partial class PlayerBombNumberChangeAnimation : Node
 {
     #region Fields
@@ -15,6 +18,9 @@ internal partial class PlayerBombNumberChangeAnimation : Node
 
     #region Overrides
 
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         PlayersBombData = GetNode<GridContainer>("%PlayersBombData");
@@ -23,6 +29,9 @@ internal partial class PlayerBombNumberChangeAnimation : Node
         Events.Instance.PlayerBombNumberDecreased += PlayerBombNumberDecreased;
     }
 
+    /// <summary>
+    /// Called when the node is removed from the scene tree.
+    /// </summary>
     protected override void Dispose(bool disposing)
     {
         Events.Instance.PlayerBombNumberIncremented -= PlayerBombNumberIncremented;
@@ -35,6 +44,11 @@ internal partial class PlayerBombNumberChangeAnimation : Node
 
     #region MethodsForSignals
 
+    /// <summary>
+    /// Called when the player bomb number is incremented.
+    /// </summary>
+    /// <param name="playerColor">The color of the player.</param>
+    /// <param name="numberOfAvailableBombs">The number of available bombs.</param>
     private void PlayerBombNumberIncremented(string playerColor, int numberOfAvailableBombs)
     {
         const float tweenScale = 1.1f;
@@ -47,6 +61,11 @@ internal partial class PlayerBombNumberChangeAnimation : Node
         PlayAnimationsForNumberIncrement(bombPicture, bombNumberCircle, tweenScale);
     }
 
+    /// <summary>
+    /// Called when the player bomb number is decreased.
+    /// </summary>
+    /// <param name="playerColor">The color of the player.</param>
+    /// <param name="numberOfAvailableBombs">The number of available bombs.</param>
     private void PlayerBombNumberDecreased(string playerColor, int numberOfAvailableBombs)
     {
         const float tweenScale = 0.85f;
@@ -61,6 +80,13 @@ internal partial class PlayerBombNumberChangeAnimation : Node
 
     #endregion
 
+    /// <summary>
+    /// Gets the necessary nodes.
+    /// </summary>
+    /// <param name="playerColor">The color of the player.</param>
+    /// <param name="bombPicture">The bomb's picture.</param>
+    /// <param name="bombNumberCircle">The bomb number circle.</param>
+    /// <returns>The bomb status container.</returns>
     private PanelContainer GetNecessaryNodes(string playerColor, out TextureRect bombPicture,
         out Panel bombNumberCircle)
     {
@@ -73,12 +99,23 @@ internal partial class PlayerBombNumberChangeAnimation : Node
         return bombStatusContainer;
     }
 
+    /// <summary>
+    /// Changes the number label text.
+    /// </summary>
+    /// <param name="numberOfAvailableBombs">The number of available bombs.</param>
+    /// <param name="bombStatusContainer">The bomb status container.</param>
     private static void ChangeNumberLabelText(int numberOfAvailableBombs, PanelContainer bombStatusContainer)
     {
         var bombNumberLabel = bombStatusContainer.GetNode<Label>("%BombNumberLabel");
         bombNumberLabel.Text = numberOfAvailableBombs.ToString();
     }
 
+    /// <summary>
+    /// Plays animations for number increment.
+    /// </summary>
+    /// <param name="bombPicture">The bomb's picture.</param>
+    /// <param name="bombNumberCircle">The bomb number circle.</param>
+    /// <param name="tweenScale">The tween scale.</param>
     private void PlayAnimationsForNumberIncrement(TextureRect bombPicture, Panel bombNumberCircle, float tweenScale)
     {
         var tween = CreateTween()
@@ -105,6 +142,13 @@ internal partial class PlayerBombNumberChangeAnimation : Node
             TweenDuration);
     }
 
+    /// <summary>
+    /// Plays animations for number decrement.
+    /// </summary>
+    /// <param name="numberOfAvailableBombs">The number of available bombs.</param>
+    /// <param name="bombNumberCircle">The bomb number circle.</param>
+    /// <param name="tweenScale">The tween scale.</param>
+    /// <param name="bombPicture">The bomb's picture.</param>
     private void PlayAnimationForNumberDecrement(int numberOfAvailableBombs, Panel bombNumberCircle, float tweenScale,
         TextureRect bombPicture)
     {
--- ui/main_ui/animation_scripts/PlayerKillAnimation.cs
+++ ui/main_ui/animation_scripts/PlayerKillAnimation.cs
@@ -3,6 +3,9 @@ using Godot;
 
 namespace Bombino.ui.main_ui.animation_scripts;
 
+/// <summary>
+/// Represents the player kill animation scene.
+/// </summary>
 internal partial class PlayerKillAnimation : Node
 {
     #region Fields
@@ -15,6 +18,9 @@ internal partial class PlayerKillAnimation : Node
 
     #region Overrides
 
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
     public override void _Ready()
     {
         PlayersBombData = GetNode<GridContainer>("%PlayersBombData");
@@ -22,6 +28,10 @@ internal partial class PlayerKillAnimation : Node
         Events.Instance.PlayerDied += OnPlayerDied;
     }
 
+    /// <summary>
+    /// Called when the node is removed from the scene tree.
+    /// </summary>
+    /// <param name="disposing">If the node is being disposed.</param>
     protected override void Dispose(bool disposing)
     {
         Events.Instance.PlayerDied -= OnPlayerDied;
@@ -33,6 +43,10 @@ internal partial class PlayerKillAnimation : Node
 
     #region MethodsForSignals
 
+    /// <summary>
+    /// Called when the player died.
+    /// </summary>
+    /// <param name="playerColor">The color of the player.</param>
     private void OnPlayerDied(string playerColor)
     {
         var playerNameContainerName = $"PlayerNameContainer_{playerColor}";
@@ -53,6 +67,11 @@ internal partial class PlayerKillAnimation : Node
 
     #endregion
 
+    /// <summary>
+    /// Plays the animation for the player kill.
+    /// </summary>
+    /// <param name="playerNameLabel">The player name label.</param>
+    /// <param name="bombPicture">The bomb's picture.</param>
     private void PlayAnimationForPlayerKill(Label playerNameLabel, TextureRect bombPicture)
     {
         var tween = CreateTween()
@@ -72,9 +91,19 @@ internal partial class PlayerKillAnimation : Node
             TweenDuration);
     }
 
+    /// <summary>
+    /// Sets the font color.
+    /// </summary>
+    /// <param name="value">The color value to set.</param>
+    /// <param name="label">The label to set the value for.</param>
     private static void SetFontColor(Color value, Label label) =>
         label.Set("theme_override_colors/font_color", value);
 
+    /// <summary>
+    /// Sets the font outline size.
+    /// </summary>
+    /// <param name="value">The outline size value to set.</param>
+    /// <param name="label">The label to set the value for.</param>
     private static void SetFontOutlineSize(int value, Label label) =>
         label.Set("theme_override_constants/outline_size", value);
 }
