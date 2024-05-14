59b442864429477af08a69022c7f6c92dffd2c47
Szekeres Márk <tkkkas@inf.elte.hu>
2024-05-14 03:24:30 +0200

    reformat file

--- game/persistence/state_resources/EnemyData.cs
+++ game/persistence/state_resources/EnemyData.cs
@@ -11,7 +11,8 @@ internal partial class EnemyData : Resource
     /// Gets or sets the position of the enemy.
     /// </summary>
     /// <returns>The position of the enemy.</returns>
-    [Export] public Vector3 Position { get; set; }
+    [Export]
+    public Vector3 Position { get; set; }
 
     /// <summary>
     /// Gets or sets a value indicating whether the enemy is dead.
@@ -43,7 +44,5 @@ internal partial class EnemyData : Resource
     /// </summary>
     /// <returns>A new instance of the <see cref="EnemyData"/> class.</returns>
     public EnemyData()
-        : this(Vector3.Zero)
-    {
-    }
+        : this(Vector3.Zero) { }
 }
