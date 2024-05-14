a8630d7c44dbf71153a428ebf6ca9515ab9229cc
Szekeres Márk <tkkkas@inf.elte.hu>
2024-05-14 15:17:21 +0200

    Add message for disabled game save loading

--- ui/load_game/SaveRow.cs
+++ ui/load_game/SaveRow.cs
@@ -21,9 +21,9 @@ internal partial class SaveRow : HBoxContainer
         var saveNameLabel = new Label { Text = baseFileName, ThemeTypeVariation = "SaveLabel" };
         SetLabelProperties(saveNameLabel);
 
-        var selectButton = new Button { Text = "Select", ThemeTypeVariation = "PrimaryButton" };
+        // disabled select button
+        var selectButton = new Button { Text = "Select", ThemeTypeVariation = "DisabledButton" };
         SetButtonThemeOverrides(selectButton);
-        // TODO: selectButton.Pressed += LoadSave
 
         var deleteButton = new Button { Text = "Delete", ThemeTypeVariation = "SecondaryButton" };
         SetButtonThemeOverrides(deleteButton);
@@ -34,9 +34,7 @@ internal partial class SaveRow : HBoxContainer
         AddChild(deleteButton);
     }
 
-    public SaveRow()
-    {
-    }
+    public SaveRow() { }
 
     /// <summary>
     /// Sets the properties of the label.
